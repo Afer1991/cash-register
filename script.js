018e8dbc3fc6ba9f4d4bdedbaae5a2f007742106
Af1991 <152735889+Afer1991@users.noreply.github.com>
2024-06-04 09:22:39 -0400

    Fix js function

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const changeDue = document.getElementById("change-due");
 const cashInput = document.getElementById("cash");
 const purchaseBtn = document.getElementById("purchase-btn");
 
-let price = 3.26;
+let price = 1.87;
 let cid = [
   ["PENNY", 1.01],
   ["NICKEL", 2.05],
@@ -20,7 +20,7 @@ const checkPurchase = () => {
   if (cash < price) {
     alert("Customer does not have enough money to purchase the item");
   } else if (cash === price) {
-    changeDue.innerText = "No change due - customer paid with exact cash";
+    changeDue.innerHTML = "<p>No change due - customer paid with exact cash</p>";
   } else {
     checkFunds(cash);
   }
@@ -33,7 +33,7 @@ const checkFunds = (input) => {
     totalCid = (totalCid * 100 + cid[i][1] * 100) / 100;
   }
   if (totalCid < change) {
-    changeDue.innerText = "Status: INSUFFICIENT_FUNDS";
+    changeDue.innerHTML = "<p>Status: INSUFFICIENT_FUNDS</p>";
   } else {
     checkForChange(change);
   }
@@ -43,7 +43,7 @@ const checkForChange = (input) => {
   let change = input;
   const cashArray = [0.01, 0.05, 0.1, 0.25, 1, 5, 10, 20, 100];
   const changeArray = [];
-  for (let i = 9; i >= 0; i--) {
+  for (let i = 8; i >= 0; i--) {
     if(change / cashArray[i] >= 1) {
       const requiredUnits = Math.floor(change / cashArray[i]);
       const availableUnits = cid[i][1] / cashArray[i];
@@ -56,9 +56,38 @@ const checkForChange = (input) => {
         change = (change - availableAmount).toFixed(2);
         changeArray.push([cid[i][0], availableAmount]);
       }
+    } else if (change / cashArray[i] < 1) {
+      changeArray.push([cid[i][0], 0]);
     }
   }
-  console.log(change, changeArray);
+  change = Number(change);
+  returnCash(change, changeArray);
+};
+
+const returnCash = (input, arr) => {
+  const reversedChangeArray = arr.reverse();
+  let totalCid = 0;
+  if(input !== 0) {
+    changeDue.innerHTML = "<p>Status: INSUFFICIENT_FUNDS</p>";
+  } else {
+    for (let i = 0; i < arr.length; i++) {
+      cid[i][1] = (cid[i][1] - arr[i][1]).toFixed(2); 
+    }
+    for (let i = 0; i < cid.length; i++) {
+      totalCid = (totalCid * 100 + cid[i][1] * 100) / 100;
+    }
+    if (totalCid === 0) {
+      changeDue.innerHTML = "<p>Status: CLOSED</p>";
+      for (let i = 8; i >= 0; i--) {
+        changeDue.innerHTML += `<p>${arr[i][0]}: $${arr[i][1]}</p>`;
+      }
+    } else {
+      changeDue.innerHTML = "<p>Status: OPEN</p>";
+      for (let i = 8; i >= 0; i--) {
+        changeDue.innerHTML += `<p>${arr[i][0]}: $${arr[i][1]}</p>`;
+      }
+    }
+  }
 };
 
 purchaseBtn.addEventListener("click", checkPurchase);
